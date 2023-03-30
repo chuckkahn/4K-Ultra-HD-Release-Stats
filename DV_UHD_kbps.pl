4f391a81f358e620717ef0b1a13422eabe327a5a
chuckkahn <chuck.kahn@post-in-toronto.on.ca>
2023-03-30 02:08:52 +0000

    now does multiple pages

--- DV_UHD_kbps.pl
+++ DV_UHD_kbps.pl
@@ -5,36 +5,56 @@ use warnings;
 use LWP::Simple;
 use Text::CSV;
 
-my $url = "https://forum.blu-ray.com/showthread.php?p=18667865";
 my $start_pattern = 'DISC INFO:';
-my $end_pattern = 'SUBTITLES:';
+my $end_pattern = '<!-- \/ message -->';
 my $csv_file = 'output.csv';
 
 my $csv = Text::CSV->new({ binary => 1, auto_diag => 1, eol => "\n" });
 open(my $fh, '>', $csv_file) or die "Cannot open $csv_file: $!";
 
 # Add header row to CSV
-$csv->print($fh, ["Disc Title", "MPEG-H HEVC Video", "* MPEG-H HEVC Video", "Dolby TrueHD/Atmos Audio"]);
+$csv->print($fh, ["Page","Disc Title", "MPEG-H HEVC Video", "* MPEG-H HEVC Video", "Dolby TrueHD/Atmos Audio"]);
 
+my %count;
 
-my $content = get($url) or die "Error getting $url: $!";
+# Loop over the page numbers
+for my $page (146..174) 
+{
 
-my @reports = split(/($start_pattern.*?$end_pattern)/s, $content);
-shift @reports; # remove first element (not a full report)
+    print "page $page =========================================================================\n";
+    # Build the URL for the current page
+    my $url = "https://forum.blu-ray.com/showthread.php?t=276448&page=$page";
 
-foreach my $report (@reports) {
-    if ( $report =~ /DISC INFO/ )
-    {
-        print "report is [$report]\n------------------------------------------\n\n\n";
-        my ($title) = $report =~ /Disc Title:\s*(.*?)\n/i;
-        $title =~ s/\r//g;
-        $title =~ s/<br \/>//g;
-        my ($vc1_bitrate) = $report =~ /MPEG-H HEVC Video.*?([\d.]+)\s*kbps/ism;
-        my ($vc2_bitrate) = $report =~ /\* MPEG-H HEVC Video.*?([\d.]+)\s*kbps/ism;
-        my ($ac1_bitrate) = $report =~ /Dolby TrueHD\/Atmos Audio.*?([\d.]+)\s*kbps/ism;
-        print "title is [$title][$vc1_bitrate][$vc2_bitrate][$ac1_bitrate]\n";
+    my $content = get($url) or die "Error getting $url: $!";
 
-        $csv->print($fh, [$title, $vc1_bitrate, $vc2_bitrate, $ac1_bitrate]);
+    # Check if the page was retrieved successfully
+    unless (defined $content) {
+        print "Error retrieving $url\n";
+        next;
+    }
+
+    my @reports = split(/($start_pattern.*?$end_pattern)/s, $content);
+    shift @reports; # remove first element (not a full report)
+
+    foreach my $report (@reports) {
+        if ( $report =~ /DISC INFO/ )
+        {
+            print "report is [$report]\n------------------------------------------\n\n\n";
+            my ($title) = $report =~ /Disc Title:\s*(.*?)\n/i;
+            $title =~ s/\r//g;
+            $title =~ s/<br \/>//g;
+            my ($vc1_bitrate) = $report =~ /MPEG-H HEVC Video.*?([\d.]+)\s*kbps/ism;
+            my ($vc2_bitrate) = $report =~ /\* MPEG-H HEVC Video.*?([\d.]+)\s*kbps/ism;
+            my ($ac1_bitrate) = $report =~ /Dolby TrueHD\/Atmos Audio.*?([\d.]+)\s*kbps/ism;
+            print "title is [$title][$vc1_bitrate][$vc2_bitrate][$ac1_bitrate]\n";
+
+            # add if title does not exist yet ?
+            $count{$title}++;
+            if ($count{$title} < 2 )
+            {
+                $csv->print($fh, [$page, $title, $vc1_bitrate, $vc2_bitrate, $ac1_bitrate]);
+            }
+        }
     }
 }
 
